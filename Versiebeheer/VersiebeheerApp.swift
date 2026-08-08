a60021dbb1a00ca73ab28701d56a94617f8f0ada
Akar Jaza <akar.jaza1212@gmail.com>
2026-08-08 14:37:19 +0300

    Initial Commit

---
+++ Versiebeheer/VersiebeheerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersiebeheerApp.swift
+//  Versiebeheer
+//
+//  Created by Akar jaza on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct VersiebeheerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
